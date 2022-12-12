549e98026a60b7c1edfb46cc48507f25b2385bea
paopaochahu <zjn28282@qq.com>
2022-12-12 18:17:45 +0800

    集成getway服务

---
+++ lvyuan-getway/src/test/java/com/zjn/lvyuan/getway/LvyuanGetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjn.lvyuan.getway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LvyuanGetwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
